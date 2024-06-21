1a8abc27ebdb7b740cd0dbeb279199a12cf575f2
Angela Rojas <angela.rojas22@gmail.com>
2024-06-21 19:40:27 -0400

    Main menu created.

---
+++ src/com/oracleone/moneyexchange/MainClass.java
@@ -0,0 +1,8 @@
+package com.oracleone.moneyexchange;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.mainMenu();
+    }
+}
---
+++ src/com/oracleone/moneyexchange/Menu.java
@@ -0,0 +1,18 @@
+package com.oracleone.moneyexchange;
+
+public class Menu {
+
+    public void mainMenu() {
+        System.out.println("**********************************************\n");
+        System.out.println("1. US Dolar(USD)       =>> Argentine Peso(ARS)");
+        System.out.println("2. Argentine Peso(ARS) =>> US Dolar(ARS)");
+        System.out.println("3. US Dolar(USD)       =>> Brazilian Real(BRL)");
+        System.out.println("4. Brazilian Real(BRL) =>> US Dolar(USD)");
+        System.out.println("5. US Dolar(USD)       =>> Colombian Peso(COP)");
+        System.out.println("6. Colombian Peso(COP) =>> US Dolar(USD)");
+        System.out.println("7. Exit\n");
+        System.out.println("Enter a valid option: ");
+        System.out.println("***********************************************");
+    }
+
+}
